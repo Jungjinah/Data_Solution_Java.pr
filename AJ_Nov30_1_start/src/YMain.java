148b8264c5751c1283a3e61fe07a5d48c9914de3
Jungjinah <96034366+Jungjinah@users.noreply.github.com>
2022-01-09 15:14:40 +0900

    Add files via upload

---
+++ AJ_Nov30_1_start/src/YMain.java
@@ -0,0 +1,25 @@
+
+public class YMain {
+	
+	/* <복습 예제>
+	 * 집까지 걸린시간
+	 * 나의 저녁 식사 메뉴
+	 * 어제 저녁에 뭐했는가~?
+	 * 취침시간은?
+	 * 첫날 소감은 어땠는가?
+	 * 앉은지 15분만에 드는 생각은?
+	 * 오늘은 며칠인가요~?
+	 */
+	
+	public static void main(String[] args) {
+		
+		System.out.printf("집까지 걸리는 시간 : %d분\t\t\n", 30);
+		System.out.printf("나의 저녁 식사 메뉴 : %s\t\t\n", "삼겹살");
+		System.out.printf("어제 저녁에 뭐했는가? : %s\t\t\n","피씨방 재미써영");
+		System.out.printf("취침시간은 : %d시%d분\t\t\n", 1, 15);
+		System.out.printf("첫날 소감은? : %s\t\t\n", "생각보다 많이 떠오르네 ㅎ");
+		System.out.printf("앉은지 15분만에 드는 생각은 : %s\t\t\n", "끝까지 간다.");
+		System.out.printf("오늘은 며칠인가 : %d년%d월%d일\t\t\n", 2021, 11, 30);
+		
+	}
+}
